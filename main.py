e03d8b2311c483fb10a16c109d3caa58927e0f19
serv <liuyuhua69@gmail.com>
2024-04-17 10:44:09 +0800

    ent_codes = ['638334323', '736482969', '333367878', 'cefeeed1-d198-4214-b1a2-9277e9f78655', '30edefb6-51af-447d-82cd-07cff070e2a2', '4f1ec7fd-74c8-4105-85c0-2cdd44307374']

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from rds02 import Rds02
 
 if __name__ == '__main__':
     # 指定账套
-    ent_codes = ['638334323', '736482969', '333367878', 'cefeeed1-d198-4214-b1a2-9277e9f78655']
+    ent_codes = ['638334323', '736482969', '333367878', 'cefeeed1-d198-4214-b1a2-9277e9f78655', '30edefb6-51af-447d-82cd-07cff070e2a2', '4f1ec7fd-74c8-4105-85c0-2cdd44307374']
 
     # 测试模式只同步前10条记录
     test_data = False
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     rds01 = Rds01(databases=['cloud_sale', 'crm', 'customer_supply', 'data_authority', 'development', 'billing',
                              'dictionary', 'form_template', 'freeze', 'hr', 'hrmis', 'mrp', 'price_center', 'cloud_finance',
                              'printer_center', 'purchase', 'rbac_new', 'supplier', 'system_setting', 'ufile_store',
-                             'unicom', 'wx_applet', 'test'])
+                             'unicom', 'wx_applet'])
     rds02 = Rds02(databases=['manufacture', 'storehouse', 'qc'])
     rds_list = [rds01, rds02]
     for rds in rds_list:
